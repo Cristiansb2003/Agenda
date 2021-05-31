fb844767303e02a3997332a6cf44c7c81b330f9e
Cristian Salazar <cristian192003@outlook.es>
2021-05-31 12:17:04 -0500

    Se agrego el boton agregar

--- js/tareas.js
+++ js/tareas.js
@@ -1,5 +1,34 @@
-$(document).ready(function(){
-   var nombre = localStorage.getItem("nombre");
+$(document).ready(function() {
+  var nombre = localStorage.getItem("nombre");
+  var contenedorTarjetas = $(".tarjetas");
+  var agregarTarjeta = $("#guardarTarea");
 
-   $("#bienvenida").text("Bienvenido "+nombre)
+  var contenedor1 = $("<div>");
+  contenedor1.addClass("card tarjetaD");
+  var titulo = $("<h5>");
+  titulo.text("Titulo");
+  titulo.addClass("card-header");
+  var contenedor2 = $("<div>");
+  contenedor2.addClass("card-body");
+  var descripcion = $("<p>");
+  descripcion.text("Descripcion");
+  descripcion.addClass("card-text");
+  contenedor1.prepend(titulo);
+  contenedor2.append(descripcion);
+  contenedor1.append(contenedor2);
+  var nuevaFila = $("<li>");
+
+
+function crearFila(){
+    nuevaFila.append(contenedor1);
+      $("#lista").append(nuevaFila);
+}
+  $("#bienvenida").text("Bienvenido " + nombre);
+
+  agregarTarjeta.click(function() {
+    console.log("hola");
+    crearFila();
+    crearFila();
+
+  });
 });
